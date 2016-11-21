fd7713c16aece6a47516e244dd9fd0122814b440
eamv-hi <hi@M12753.ceuherning.int>
2016-11-21 10:35:14 +0100

    Konto udviddet

--- bank/src/domain/Konto.java
+++ bank/src/domain/Konto.java
@@ -1,5 +1,40 @@
 package domain;
 
+import java.math.BigDecimal;
+
 public class Konto {
+	private int regnr;
+	private int kontonr;
+	private int kontotype;
+	private BigDecimal saldo;
+	
+	public int getRegnr() {
+		return regnr;
+	}
+	public Konto setRegnr(int regnr) {
+		this.regnr = regnr;
+		return this;
+	}
+	public int getKontonr() {
+		return kontonr;
+	}
+	public Konto setKontonr(int kontonr) {
+		this.kontonr = kontonr;
+		return this;
+	}
+	public int getKontotype() {
+		return kontotype;
+	}
+	public Konto setKontotype(int kontotype) {
+		this.kontotype = kontotype;
+		return this;
+	}
+	public BigDecimal getSaldo() {
+		return saldo;
+	}
+	public Konto setSaldo(BigDecimal saldo) {
+		this.saldo = saldo;
+		return this;
+	}
 
 }
